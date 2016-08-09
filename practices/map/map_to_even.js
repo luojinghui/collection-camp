d0a81a433018b6545a344b1bdbade60b96d0d0ec
luojinghui <939035243@qq.com>
2016-08-09 12:44:36 +0800

    add map-to-even-spec.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,9 +1,22 @@
 'use strict';
-function map_to_even(collection){
-    var _ = require('../lodash/mylodash.js');
 
-    return _.map(collection,function(n) {
-        return n * 2;
-});
+// var _ = require('../../tools/prolodash.js');
+var _ = require('../../tools/prolodash');
+function map_to_even(collection){
+    // return _.map(collection,function(n) {
+    //     return n * 2;
+    // });
+
+    return _(collection).map(item => item * 2).value();
 }
+// function map(array, fun) {
+//     var result = [];
+//
+//     _.each(array, item => {
+//         result.push(fun(item));
+//     })
+//
+//     return result;
+// }
+
 module.exports = map_to_even;
