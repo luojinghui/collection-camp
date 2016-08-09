1849b17160ecee53048979e110ee297caec36baa
luojinghui <939035243@qq.com>
2016-08-09 12:51:46 +0800

    map-to-four-multiples-add-one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,9 +1,13 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-    var _ = require('../lodash/mylodash.js');
+    // var _ = require('../lodash/mylodash.js');
+    //
+    // return _.map(collection,function(n) {
+    //     return n * 4 + 1;
+    // });
 
-    return _.map(collection,function(n) {
-        return n * 4 + 1;
-    });
+    var _ = require('../../tools/prolodash');
+
+    return _(collection).map(item => item * 4 + 1).value();
 }
 module.exports = map_to_four_multiples_add_one;
